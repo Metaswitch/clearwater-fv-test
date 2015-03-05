5f769ae381ef8ff07802e123832edb961c745f46
Alex Hockey <alex.hockey@metaswitch.com>
2015-03-05 14:11:56 +0000

    Add some tests for config file reading

---
+++ src/test_memcached_config.cpp
@@ -0,0 +1,128 @@
+/**
+ * @file test_memcached_config.cpp Test reading a memcached config file.
+ *
+ * Project Clearwater - IMS in the cloud.
+ * Copyright (C) 2015  Metaswitch Networks Ltd
+ *
+ * This program is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the
+ * Free Software Foundation, either version 3 of the License, or (at your
+ * option) any later version, along with the "Special Exception" for use of
+ * the program along with SSL, set forth below. This program is distributed
+ * in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE.  See the GNU General Public License for more
+ * details. You should have received a copy of the GNU General Public
+ * License along with this program.  If not, see
+ * <http://www.gnu.org/licenses/>.
+ *
+ * The author can be reached by email at clearwater@metaswitch.com or by
+ * post at Metaswitch Networks Ltd, 100 Church St, Enfield EN2 6BQ, UK
+ *
+ * Special Exception
+ * Metaswitch Networks Ltd  grants you permission to copy, modify,
+ * propagate, and distribute a work formed by combining OpenSSL with The
+ * Software, or a work derivative of such a combination, even if such
+ * copying, modification, propagation, or distribution would otherwise
+ * violate the terms of the GPL. You must comply with the GPL in all
+ * respects for all of the code used other than OpenSSL.
+ * "OpenSSL" means OpenSSL toolkit software distributed by the OpenSSL
+ * Project and licensed under the OpenSSL Licenses, or a work based on such
+ * software and licensed under the OpenSSL Licenses.
+ * "OpenSSL Licenses" means the OpenSSL License and Original SSLeay License
+ * under which the OpenSSL Project distributes the OpenSSL toolkit software,
+ * as those licenses appear in the file LICENSE-OPENSSL.
+ */
+
+#include <fstream>
+
+#include "gmock/gmock.h"
+#include "gtest/gtest.h"
+
+#include "memcached_config.h"
+
+class MemcachedConfigTest : public ::testing::Test
+{
+public:
+  std::string _filename;
+  MemcachedConfigReader* _reader;
+
+  virtual void SetUp()
+  {
+    _filename = "./cluster_settings." + std::to_string(getpid());
+    _reader = new MemcachedConfigFileReader(_filename);
+  }
+
+  virtual void TearDown()
+  {
+    delete _reader; _reader = NULL;
+    ::unlink(_filename.c_str());
+  }
+
+  void write_config(const std::string cfg)
+  {
+    std::ofstream f(_filename);
+    EXPECT_TRUE(f.is_open());
+    f << cfg;
+  }
+};
+
+TEST_F(MemcachedConfigTest, AllSettings)
+{
+  write_config("servers=192.168.0.1:11211,192.168.0.2:11211\n"
+               "new_servers=10.0.0.1:11211\n"
+               "tombstone_lifetime=200");
+
+  MemcachedConfig config;
+  EXPECT_TRUE(_reader->read_config(config));
+  EXPECT_EQ(config.servers.size(), 2u);
+  EXPECT_EQ(config.servers[0], "192.168.0.1:11211");
+  EXPECT_EQ(config.servers[1], "192.168.0.2:11211");
+  EXPECT_EQ(config.new_servers.size(), 1u);
+  EXPECT_EQ(config.new_servers[0], "10.0.0.1:11211");
+  EXPECT_EQ(config.tombstone_lifetime, 200);
+}
+
+
+TEST_F(MemcachedConfigTest, OptionalFieldsEmpty)
+{
+  write_config("servers=192.168.0.1:11211,192.168.0.2:11211");
+
+  MemcachedConfig config;
+  EXPECT_TRUE(_reader->read_config(config));
+  EXPECT_EQ(config.servers.size(), 2u);
+  EXPECT_EQ(config.servers[0], "192.168.0.1:11211");
+  EXPECT_EQ(config.servers[1], "192.168.0.2:11211");
+  // new_servers defaults to an empty vector.
+  EXPECT_EQ(config.new_servers.size(), 0u);
+  // tombstone_lifetime defaults to 0 at present (meaning don't use tombstones).
+  EXPECT_EQ(config.tombstone_lifetime, 0);
+}
+
+
+TEST_F(MemcachedConfigTest, CorruptConfig)
+{
+  write_config("servers");
+
+  MemcachedConfig config;
+  EXPECT_FALSE(_reader->read_config(config));
+}
+
+
+TEST_F(MemcachedConfigTest, ServerListEmpty)
+{
+  write_config("servers=");
+
+  MemcachedConfig config;
+  EXPECT_FALSE(_reader->read_config(config));
+}
+
+
+TEST_F(MemcachedConfigTest, BadTombstoneLifetime)
+{
+  write_config("servers=192.168.0.1:11211\n"
+               "tombstone_lifetime=200s"); // Trailing characters
+
+  MemcachedConfig config;
+  EXPECT_FALSE(_reader->read_config(config));
+}
