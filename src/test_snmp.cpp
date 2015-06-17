1ea63312ffce67f9aadc614d34b95706f6ed102f
Rob Day <Robert.Day@metaswitch.com>
2015-06-17 18:11:32 +0000

    Tweak UT descriptions

--- src/test_snmp.cpp
+++ src/test_snmp.cpp
@@ -54,6 +54,7 @@ static void* snmp_thread(void* data)
 
 
 static pthread_t thr;
+static oid test_oid[] = { 1, 2, 2 };
 
 class SNMPTest : public ::testing::Test
 {
@@ -61,6 +62,7 @@ class SNMPTest : public ::testing::Test
 
   static void SetUpTestCase()
   {
+    // Configure SNMPd to use the fvtest.conf in the local directory
     char cwd[256];
     getcwd(cwd, sizeof(cwd));
     netsnmp_ds_set_string(NETSNMP_DS_LIBRARY_ID,
@@ -70,11 +72,14 @@ class SNMPTest : public ::testing::Test
                           NETSNMP_DS_LIB_MIBDIRS,
                           "");
 
+    // Log SNMPd output to a file
     snmp_enable_filelog("fvtest-snmpd.out", 0);
+
     init_agent("fvtest");
     init_snmp("fvtest");
     init_master_agent();
 
+    // Run a thread to handle SNMP requests
     pthread_create(&thr, NULL, snmp_thread, NULL);
   }
   
@@ -89,24 +94,31 @@ class SNMPTest : public ::testing::Test
 
 TEST_F(SNMPTest, ScalarValue)
 {
-  oid test_oid[] = { 1, 2, 2 };
+  // Create a scalar
   SNMP::U32Scalar scalar("answer", test_oid, OID_LENGTH(test_oid));
   scalar.value = 42;
 
+  // Shell out to snmpget to query that scalar
   FILE* fd = popen("snmpget -v2c -On -c clearwater 127.0.0.1:16161 .1.2.2", "r");
   char buf[1024];
   fgets(buf, sizeof(buf), fd);
+
+  // Check that it has the right OID, value and type
   ASSERT_STREQ(".1.2.2 = Gauge32: 42\n", buf);
 }
 
 
 TEST_F(SNMPTest, TableOrdering)
 {
-  oid test_oid[] = { 1, 2, 2 };
+  // Create a table
   SNMP::AccumulatorTable tbl("latency", test_oid, OID_LENGTH(test_oid));
 
+  // Shell out to snmpwalk to find all entries in that table
   FILE* fd = popen("snmpwalk -v2c -On -c clearwater 127.0.0.1:16161 .1.2.2", "r");
   char buf[1024];
+
+  // Check that they come in the right order - column 2 of row 0, column 2 of row 1, column 3 of row
+  // 0, column 3 of row 1.
   fgets(buf, sizeof(buf), fd);
   ASSERT_STREQ(".1.2.2.1.2.0 = Gauge32: 0\n", buf);
   fgets(buf, sizeof(buf), fd);
