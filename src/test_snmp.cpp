f2421703209dda13bdc91325ceda2a1bd709e65b
Rob Day <Robert.Day@metaswitch.com>
2015-06-26 12:09:59 +0000

    Improve comments

--- src/test_snmp.cpp
+++ src/test_snmp.cpp
@@ -140,9 +140,10 @@ TEST_F(SNMPTest, LatencyCalculations)
   // Create a table
   SNMP::AccumulatorTable tbl("latency", test_oid, OID_LENGTH(test_oid));
 
+  // Just put one sample in (which should have a variance of 0).
   tbl.accumulate(100);
 
-  // Move on five seconds. The "previous five seconds" stat should now also reflect the increment.
+  // Move on five seconds. The "previous five seconds" stat should now reflect that sample.
   cwtest_advance_time_ms(5000);
 
   // Average should be 100
@@ -154,10 +155,11 @@ TEST_F(SNMPTest, LatencyCalculations)
   fgets(buf, sizeof(buf), fd);
   ASSERT_STREQ(".1.2.2.1.3.1 = Gauge32: 0\n", buf);
 
+  // Now input two samples in this latency period.
   tbl.accumulate(300);
   tbl.accumulate(500);
 
-  // Move on five seconds. The "previous five seconds" stat should now also reflect the increment.
+  // Move on five seconds. The "previous five seconds" stat should now reflect those two samples.
   cwtest_advance_time_ms(5000);
 
   // Average should be 400
@@ -177,13 +179,8 @@ TEST_F(SNMPTest, LatencyCalculations)
   fgets(buf, sizeof(buf), fd);
   ASSERT_STREQ(".1.2.2.1.6.1 = Gauge32: 2\n", buf);
 
-
-
-
   cwtest_reset_time();
 }
-
-
 
 TEST_F(SNMPTest, CounterTimePeriods)
 {
