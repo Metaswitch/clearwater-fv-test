70c408534f9cd5c8438ae246a4c0a0a8d20eb090
Graeme Robertson <graeme.robertson@metaswitch.com>
2015-12-01 18:56:46 +0000

    Build Astaire

---
+++ mk/astaire.mk
@@ -0,0 +1,16 @@
+# included mk file for astaire
+
+ASTAIRE_DIR := ${MODULE_DIR}/astaire
+
+astaire :
+	ROOT=${ASTAIRE_DIR} ${MAKE} -C ${ASTAIRE_DIR}
+
+astaire_test :
+	ROOT=${ASTAIRE_DIR} ${MAKE} -C ${ASTAIRE_DIR} test
+
+astaire_clean :
+	ROOT=${ASTAIRE_DIR} ${MAKE} -C ${ASTAIRE_DIR} clean
+
+astaire_distclean: astaire_clean
+
+.PHONY : astaire astaire_test astaire_clean astaire_distclean
